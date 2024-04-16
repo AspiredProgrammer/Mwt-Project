dc87fdc1f53cb101fa6782f72e31f9f924de2a91
liploji <zeldrix.don@gmail.com>
2024-04-15 22:09:14 -0400

    edited the Navbar created a ProductCard edited HomePage edited ProductPage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import Navbar from "../components/Navbar";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = ({ products }) => {
   const [userName, setUserName] = useState("");
@@ -18,27 +19,18 @@ const HomePage = ({ products }) => {
     <div className="index-container">
       <Navbar />
       {userName && <p>Hello, {userName}!</p>}
-      {products.map((product) => (
-        <div key={product._id} className="book-container">
-          <img src={product.image} width={100} alt="product_image"/>
-          <div className="title-box">
-            <label className="book-title">
-              <a
-                href={
-                  userName.length > 0 ? `/product/${product._id}` : "/login"
-                }
-                className="title-link"
-              >
-                {product.title}
-              </a>
-            </label>
-          </div>
-          <div className="info-box">
-            <p className="book-info">CAD$ {product.price.toFixed(2)}</p>
-            <p className="book-info">Rating: {product.rating} / 5</p>
-          </div>
-        </div>
-      ))}
+      <div
+        className="product-grid"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          gap: "20px",
+        }}
+      >
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
       <br />
       <br />
     </div>
